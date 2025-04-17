89f6b62245df0f528077919e0816ed00bff8760a
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-04-17 18:38:30 +0600

    solve some problem

--- ProblemSolve2/practice5.c
+++ ProblemSolve2/practice5.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 int main(){
-    int a[5];
-    int i = 0;
-    while (i < 5)
-    {
-        scanf("%d",&a[i]);
-        i++;
-        /* code */
-    }
-    i = 0;
-    do
-    {
-        /* code */
-        printf("%d ",a[i]);
-        i++;
-        if(i == 5){
-            break;
-        }
+    // int a[5];
+    // int i = 0;
+    // while (i < 5)
+    // {
+    //     scanf("%d",&a[i]);
+    //     i++;
+    //     /* code */
+    // }
+    // i = 0;
+    // do
+    // {
+    //     /* code */
+    //     printf("%d ",a[i]);
+    //     i++;
+    //     if(i == 5){
+    //         break;
+    //     }
        
-    }while (i < 5);
-    
+    // }while (i < 5);
+    int arrayName[5] = {1,2 , 4};
+    printf("%d",arrayName[0]);
     return 0;
 }
